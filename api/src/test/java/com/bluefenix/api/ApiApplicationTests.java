5affacc561b6546efd8ef00acf2bd39f02610972
Scoorpioon <gabrielalexandre1337@gmail.com>
2024-09-14 11:51:40 -0300

    Base da API

---
+++ api/src/test/java/com/bluefenix/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluefenix.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
